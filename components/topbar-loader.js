a3d645ee53a730c5389fd3d0b852326feb6900bf
SatoshiumAI <satoshiumai@pm.me>
2026-04-02 16:01:18 -0700

    Update topbar-loader.js

--- components/topbar-loader.js
+++ components/topbar-loader.js
@@ -117,6 +117,12 @@
     return;
   }
 
+     if (
+    hostIs("satoshium.games", "www.satoshium.games")) {
+    setActive("games");
+    return;
+  }
+
   if (hostIs("satoshium.store", "www.satoshium.store")) {
     setActive("store");
     return;
